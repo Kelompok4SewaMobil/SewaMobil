cc099523b21cc1369c5a908af9de3e4193e2ec43
umirfh <umirofifah21@gmail.com>
2022-01-04 16:51:24 +0700

    menambahkan button update pada form akun

--- FPSewaMobil/Form1.Designer.cs
+++ FPSewaMobil/Form1.Designer.cs
@@ -35,77 +35,91 @@ namespace FPSewaMobil
             this.label1 = new System.Windows.Forms.Label();
             this.dgvuser = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvuser)).BeginInit();
             this.SuspendLayout();
             // 
             // txtpwd
             // 
             this.txtpwd.Font = new System.Drawing.Font("Wingdings", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.txtpwd.Location = new System.Drawing.Point(174, 112);
-            this.txtpwd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtpwd.Location = new System.Drawing.Point(116, 73);
+            this.txtpwd.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.txtpwd.Name = "txtpwd";
             this.txtpwd.PasswordChar = 'l';
-            this.txtpwd.Size = new System.Drawing.Size(224, 25);
+            this.txtpwd.Size = new System.Drawing.Size(151, 19);
             this.txtpwd.TabIndex = 11;
             // 
             // txtuser
             // 
-            this.txtuser.Location = new System.Drawing.Point(174, 65);
-            this.txtuser.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtuser.Location = new System.Drawing.Point(116, 42);
+            this.txtuser.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.txtuser.Name = "txtuser";
-            this.txtuser.Size = new System.Drawing.Size(224, 26);
+            this.txtuser.Size = new System.Drawing.Size(151, 20);
             this.txtuser.TabIndex = 10;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(39, 119);
+            this.label2.Location = new System.Drawing.Point(26, 77);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 9;
             this.label2.Text = "Password";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 65);
+            this.label1.Location = new System.Drawing.Point(24, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "User Id";
             // 
             // dgvuser
             // 
             this.dgvuser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvuser.Location = new System.Drawing.Point(39, 264);
-            this.dgvuser.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dgvuser.Location = new System.Drawing.Point(26, 172);
+            this.dgvuser.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.dgvuser.Name = "dgvuser";
             this.dgvuser.RowHeadersWidth = 51;
             this.dgvuser.RowTemplate.Height = 24;
-            this.dgvuser.Size = new System.Drawing.Size(360, 245);
+            this.dgvuser.Size = new System.Drawing.Size(240, 159);
             this.dgvuser.TabIndex = 6;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(43, 174);
+            this.button1.Location = new System.Drawing.Point(29, 113);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(74, 46);
+            this.button1.Size = new System.Drawing.Size(49, 30);
             this.button1.TabIndex = 12;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(116, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 29);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(476, 590);
+            this.ClientSize = new System.Drawing.Size(317, 383);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtpwd);
             this.Controls.Add(this.txtuser);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvuser);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "Form1";
             this.Text = "Form Data Akun";
             ((System.ComponentModel.ISupportInitialize)(this.dgvuser)).EndInit();
@@ -121,6 +135,7 @@ namespace FPSewaMobil
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dgvuser;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
