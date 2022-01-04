a09dc38641fd7a735d61343df0ef303aaba7a945
putri <putriamalliaramadhani@gmail.com>
2022-01-04 17:28:06 +0700

    membuat kode program untuk mengenkripsi data akun

---
+++ FPSewaMobil/Form Data Akun.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace FPSewaMobil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtpwd = new System.Windows.Forms.TextBox();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvuser = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvuser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtpwd
+            // 
+            this.txtpwd.Font = new System.Drawing.Font("Wingdings", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.txtpwd.Location = new System.Drawing.Point(155, 90);
+            this.txtpwd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtpwd.Name = "txtpwd";
+            this.txtpwd.PasswordChar = 'l';
+            this.txtpwd.Size = new System.Drawing.Size(200, 22);
+            this.txtpwd.TabIndex = 11;
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(155, 52);
+            this.txtuser.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(200, 22);
+            this.txtuser.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "User Id";
+            // 
+            // dgvuser
+            // 
+            this.dgvuser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvuser.Location = new System.Drawing.Point(35, 212);
+            this.dgvuser.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dgvuser.Name = "dgvuser";
+            this.dgvuser.RowHeadersWidth = 51;
+            this.dgvuser.RowTemplate.Height = 24;
+            this.dgvuser.Size = new System.Drawing.Size(320, 196);
+            this.dgvuser.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 139);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 37);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(155, 140);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 36);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(280, 140);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 36);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 471);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtpwd);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvuser);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form Data Akun";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvuser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtpwd;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvuser;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ FPSewaMobil/Form Data Akun.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FPSewaMobil
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection
+        (@"Data Source=LAPTOP-S7KKHH8P;Initial Catalog=SEWA_MOBIL;Integrated Security=true");
+
+        private void resetdata()
+        {
+            txtuser.Text = "";
+            txtpwd.Text = "";
+        }
+        private void showdata()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from admin";
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds, "admin");
+            dgvuser.DataSource = ds;
+            dgvuser.DataMember = "admin";
+            dgvuser.ReadOnly = true;
+        }
+        private string CaesarCipher(string value, int shift)
+        {
+            string[] joinCipher = new string[200];
+            string joinText = "";
+            string[] wordArray = value.Split(' ');
+            try
+            {
+                for (int x = 0; x < wordArray.Length; x++)
+                {
+                    char[] buffer = wordArray[x].ToCharArray();
+                    for (int i = 0; i < buffer.Length; i++)
+                    {
+                        char letter = buffer[i];
+                        letter = (char)(letter + shift);
+                        if(letter > 'z')
+                        {
+                            letter = (char)(letter - 26);
+                        }
+                        else if (letter < 'a')
+                        {
+                            letter = (char)(letter + 26);
+                        }
+                        buffer[i] = letter;
+                    }
+                    string HasilKonversi = new string(buffer);
+                    joinCipher[x] = HasilKonversi;
+                }
+                joinText = string.Join(" ", joinCipher);
+                return joinText;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return null;
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
