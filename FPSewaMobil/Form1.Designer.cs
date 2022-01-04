d7524562c6982465f3cffc428b92b4a98353c485
putriamall <96557494+putriamall@users.noreply.github.com>
2022-01-04 16:44:37 +0700

    Menambahkan Form Data Akun
    Menambahkan user, password, dan data gridview

---
+++ FPSewaMobil/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace FPSewaMobil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtpwd = new System.Windows.Forms.TextBox();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvuser = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvuser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtpwd
+            // 
+            this.txtpwd.Font = new System.Drawing.Font("Wingdings", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.txtpwd.Location = new System.Drawing.Point(155, 90);
+            this.txtpwd.Name = "txtpwd";
+            this.txtpwd.PasswordChar = 'l';
+            this.txtpwd.Size = new System.Drawing.Size(200, 22);
+            this.txtpwd.TabIndex = 11;
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(155, 52);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(200, 22);
+            this.txtuser.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "User Id";
+            // 
+            // dgvuser
+            // 
+            this.dgvuser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvuser.Location = new System.Drawing.Point(35, 211);
+            this.dgvuser.Name = "dgvuser";
+            this.dgvuser.RowHeadersWidth = 51;
+            this.dgvuser.RowTemplate.Height = 24;
+            this.dgvuser.Size = new System.Drawing.Size(320, 196);
+            this.dgvuser.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 472);
+            this.Controls.Add(this.txtpwd);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvuser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvuser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtpwd;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvuser;
+    }
+}
+
