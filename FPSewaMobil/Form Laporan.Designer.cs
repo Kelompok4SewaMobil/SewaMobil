629bfd527db04bf61fe2b7392fd3fa360fd99787
umirfh <umirofifah21@gmail.com>
2022-01-06 09:27:43 +0700

    menambahkan dataGridView pada form laporan

--- FPSewaMobil/Form Laporan.Designer.cs
+++ FPSewaMobil/Form Laporan.Designer.cs
@@ -30,25 +30,37 @@ namespace FPSewaMobil
         private void InitializeComponent()
         {
             this.print = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // print
             // 
-            this.print.Location = new System.Drawing.Point(25, 31);
+            this.print.Location = new System.Drawing.Point(25, 37);
             this.print.Name = "print";
             this.print.Size = new System.Drawing.Size(100, 33);
             this.print.TabIndex = 0;
             this.print.Text = "Print";
             this.print.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 93);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(402, 325);
+            this.dataGridView1.TabIndex = 1;
+            // 
             // Form_Laporan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(455, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.print);
             this.Name = "Form_Laporan";
             this.Text = "Form_Laporan";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +68,6 @@ namespace FPSewaMobil
         #endregion
 
         private System.Windows.Forms.Button print;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
