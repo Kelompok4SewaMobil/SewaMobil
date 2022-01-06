1b033b66e1f49dfe59d669efcaa6f0ea3a5d17a2
umirfh <umirofifah21@gmail.com>
2022-01-06 15:48:00 +0700

    menambahkan button kembali pada form laporan

--- FPSewaMobil/Form Laporan.Designer.cs
+++ FPSewaMobil/Form Laporan.Designer.cs
@@ -31,6 +31,7 @@ namespace FPSewaMobil
         {
             this.print = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.kembali = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,11 +53,21 @@ namespace FPSewaMobil
             this.dataGridView1.Size = new System.Drawing.Size(402, 325);
             this.dataGridView1.TabIndex = 1;
             // 
+            // kembali
+            // 
+            this.kembali.Location = new System.Drawing.Point(327, 37);
+            this.kembali.Name = "kembali";
+            this.kembali.Size = new System.Drawing.Size(100, 33);
+            this.kembali.TabIndex = 2;
+            this.kembali.Text = "Kembali";
+            this.kembali.UseVisualStyleBackColor = true;
+            // 
             // Form_Laporan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(455, 450);
+            this.Controls.Add(this.kembali);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.print);
             this.Name = "Form_Laporan";
@@ -71,5 +82,6 @@ namespace FPSewaMobil
 
         private System.Windows.Forms.Button print;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button kembali;
     }
 }
