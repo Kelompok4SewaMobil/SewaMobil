aaf98bc7aa41952aec45288b36c1b76618e122ba
umirfh <umirofifah21@gmail.com>
2022-01-06 09:21:54 +0700

    menambahkan button print pada form laporan

--- FPSewaMobil/Form Laporan.Designer.cs
+++ FPSewaMobil/Form Laporan.Designer.cs
@@ -29,13 +29,24 @@ namespace FPSewaMobil
         /// </summary>
         private void InitializeComponent()
         {
+            this.print = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(25, 31);
+            this.print.Name = "print";
+            this.print.Size = new System.Drawing.Size(100, 33);
+            this.print.TabIndex = 0;
+            this.print.Text = "Print";
+            this.print.UseVisualStyleBackColor = true;
             // 
             // Form_Laporan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(455, 450);
+            this.Controls.Add(this.print);
             this.Name = "Form_Laporan";
             this.Text = "Form_Laporan";
             this.ResumeLayout(false);
@@ -43,5 +54,7 @@ namespace FPSewaMobil
         }
 
         #endregion
+
+        private System.Windows.Forms.Button print;
     }
 }
