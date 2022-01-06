1242f6b0df2e09e1a77ad193a8982bee0762d24a
putri <putriamalliaramadhani@gmail.com>
2022-01-06 16:57:51 +0700

    menambahkan deskripsi kelompok dan menambahkan button kembali pada form about kelompok

--- FPSewaMobil/Form_AboutKelompok.Designer.cs
+++ FPSewaMobil/Form_AboutKelompok.Designer.cs
@@ -29,12 +29,59 @@ namespace FPSewaMobil
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_AboutKelompok));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.aboutkembali2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(30, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(729, 342);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(379, 238);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // aboutkembali2
+            // 
+            this.aboutkembali2.Location = new System.Drawing.Point(620, 391);
+            this.aboutkembali2.Name = "aboutkembali2";
+            this.aboutkembali2.Size = new System.Drawing.Size(139, 32);
+            this.aboutkembali2.TabIndex = 1;
+            this.aboutkembali2.Text = "Kembali";
+            this.aboutkembali2.UseVisualStyleBackColor = true;
+            // 
+            // Form_AboutKelompok
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.aboutkembali2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form_AboutKelompok";
             this.Text = "Form_AboutKelompok";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button aboutkembali2;
     }
 }
