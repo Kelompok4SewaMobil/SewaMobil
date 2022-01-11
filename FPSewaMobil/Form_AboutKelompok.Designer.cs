a13b4f491d7d80c0a8e0c78c60a832464ebf2f34
putri <putriamalliaramadhani@gmail.com>
2022-01-11 18:35:40 +0700

    mengubah tampilan about kelompok

--- FPSewaMobil/Form_AboutKelompok.Designer.cs
+++ FPSewaMobil/Form_AboutKelompok.Designer.cs
@@ -30,64 +30,48 @@ namespace FPSewaMobil
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_AboutKelompok));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.aboutkembali2 = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(34, 34);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox1.Size = new System.Drawing.Size(820, 428);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(184, 49);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(425, 280);
-            this.label1.TabIndex = 0;
-            this.label1.Text = resources.GetString("label1.Text");
             // 
             // aboutkembali2
             // 
-            this.aboutkembali2.Location = new System.Drawing.Point(698, 489);
-            this.aboutkembali2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.aboutkembali2.Location = new System.Drawing.Point(386, 473);
             this.aboutkembali2.Name = "aboutkembali2";
-            this.aboutkembali2.Size = new System.Drawing.Size(156, 40);
+            this.aboutkembali2.Size = new System.Drawing.Size(139, 32);
             this.aboutkembali2.TabIndex = 1;
             this.aboutkembali2.Text = "Kembali";
             this.aboutkembali2.UseVisualStyleBackColor = true;
             this.aboutkembali2.Click += new System.EventHandler(this.aboutkembali2_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(913, 555);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form_AboutKelompok
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1028, 694);
+            this.ClientSize = new System.Drawing.Size(914, 555);
             this.Controls.Add(this.aboutkembali2);
-            this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form_AboutKelompok";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "About Kelompok";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button aboutkembali2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
