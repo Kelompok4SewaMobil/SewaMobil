e294c92044903250ccb5912fc063a76453344130
putri <putriamalliaramadhani@gmail.com>
2022-01-11 18:33:36 +0700

    mengubah tampilan about aplikasi

--- FPSewaMobil/Form_AboutAplikasi.Designer.cs
+++ FPSewaMobil/Form_AboutAplikasi.Designer.cs
@@ -29,65 +29,51 @@ namespace FPSewaMobil
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_AboutAplikasi));
             this.aboutkembali1 = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(20, 20);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox1.Size = new System.Drawing.Size(654, 583);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 12);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Deskripsi Aplikasi";
             // 
             // aboutkembali1
             // 
-            this.aboutkembali1.Location = new System.Drawing.Point(478, 614);
-            this.aboutkembali1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.aboutkembali1.Location = new System.Drawing.Point(383, 454);
+            this.aboutkembali1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.aboutkembali1.Name = "aboutkembali1";
-            this.aboutkembali1.Size = new System.Drawing.Size(194, 60);
+            this.aboutkembali1.Size = new System.Drawing.Size(172, 48);
             this.aboutkembali1.TabIndex = 1;
             this.aboutkembali1.Text = "Kembali";
             this.aboutkembali1.UseVisualStyleBackColor = true;
             this.aboutkembali1.Click += new System.EventHandler(this.aboutkembali1_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(915, 556);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form_AboutAplikasi
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1028, 694);
+            this.ClientSize = new System.Drawing.Size(914, 555);
             this.Controls.Add(this.aboutkembali1);
-            this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form_AboutAplikasi";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "About Aplikasi";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button aboutkembali1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
