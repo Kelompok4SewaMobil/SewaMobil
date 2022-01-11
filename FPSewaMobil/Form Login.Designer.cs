1390d5218520039fa0fca8b6cd9125b0df55910e
putri <putriamalliaramadhani@gmail.com>
2022-01-11 16:02:15 +0700

    mengubah tampilan UI/UX

--- FPSewaMobil/Form Login.Designer.cs
+++ FPSewaMobil/Form Login.Designer.cs
@@ -29,119 +29,76 @@ namespace FPSewaMobil
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Login));
             this.txtpwd = new System.Windows.Forms.TextBox();
             this.txtuser = new System.Windows.Forms.TextBox();
             this.btnlogin = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.keluar = new System.Windows.Forms.Button();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtpwd
             // 
-            this.txtpwd.Location = new System.Drawing.Point(781, 356);
-            this.txtpwd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtpwd.Location = new System.Drawing.Point(666, 416);
             this.txtpwd.Name = "txtpwd";
             this.txtpwd.PasswordChar = '*';
-            this.txtpwd.Size = new System.Drawing.Size(200, 26);
+            this.txtpwd.Size = new System.Drawing.Size(298, 22);
             this.txtpwd.TabIndex = 9;
             this.txtpwd.TextChanged += new System.EventHandler(this.txtpwd_TextChanged);
             // 
             // txtuser
             // 
-            this.txtuser.Location = new System.Drawing.Point(781, 302);
-            this.txtuser.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtuser.Location = new System.Drawing.Point(666, 327);
             this.txtuser.Name = "txtuser";
-            this.txtuser.Size = new System.Drawing.Size(200, 26);
+            this.txtuser.Size = new System.Drawing.Size(298, 22);
             this.txtuser.TabIndex = 8;
             this.txtuser.TextChanged += new System.EventHandler(this.txtuser_TextChanged);
             // 
             // btnlogin
             // 
-            this.btnlogin.Location = new System.Drawing.Point(781, 401);
-            this.btnlogin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnlogin.Image = ((System.Drawing.Image)(resources.GetObject("btnlogin.Image")));
+            this.btnlogin.Location = new System.Drawing.Point(724, 469);
             this.btnlogin.Name = "btnlogin";
-            this.btnlogin.Size = new System.Drawing.Size(200, 41);
+            this.btnlogin.Size = new System.Drawing.Size(178, 33);
             this.btnlogin.TabIndex = 7;
-            this.btnlogin.Text = "LOGIN";
             this.btnlogin.UseVisualStyleBackColor = true;
             this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(791, 332);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 20);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Password";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(786, 278);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 20);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Username";
-            // 
             // keluar
             // 
-            this.keluar.Location = new System.Drawing.Point(781, 450);
-            this.keluar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.keluar.Image = ((System.Drawing.Image)(resources.GetObject("keluar.Image")));
+            this.keluar.Location = new System.Drawing.Point(724, 524);
             this.keluar.Name = "keluar";
-            this.keluar.Size = new System.Drawing.Size(200, 42);
+            this.keluar.Size = new System.Drawing.Size(178, 34);
             this.keluar.TabIndex = 10;
-            this.keluar.Text = "KELUAR";
             this.keluar.UseVisualStyleBackColor = true;
             this.keluar.Click += new System.EventHandler(this.keluar_Click);
             // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.pictureBox2.Image = global::FPSewaMobil.Properties.Resources.Lamborghini_logo;
-            this.pictureBox2.Location = new System.Drawing.Point(769, 30);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(234, 212);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 12;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::FPSewaMobil.Properties.Resources._6798953;
-            this.pictureBox1.Location = new System.Drawing.Point(1, 3);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(741, 513);
+            this.pictureBox1.Size = new System.Drawing.Size(1035, 701);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 11;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form_Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1015, 519);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1032, 703);
             this.Controls.Add(this.keluar);
             this.Controls.Add(this.txtpwd);
-            this.Controls.Add(this.txtuser);
             this.Controls.Add(this.btnlogin);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form_Login";
             this.Text = "Form_Login";
             this.Load += new System.EventHandler(this.Form_Login_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -153,10 +110,7 @@ namespace FPSewaMobil
         private System.Windows.Forms.TextBox txtpwd;
         private System.Windows.Forms.TextBox txtuser;
         private System.Windows.Forms.Button btnlogin;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button keluar;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
